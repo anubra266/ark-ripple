be7d1a4da9836f4f8c02c1455c45fe27a81e88c3
anubra266 <anubra266@gmail.com>
2026-03-03 02:26:57 -0800

    Update PolymorphicProps type definition and refactor use-virtualizer.ripple for improved cleanup handling

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,7 @@ export type HTMLProps<E extends ElementType> = JSX.IntrinsicElements[E];
 
 type ParentProps<E extends ElementType> = (userProps?: JSX.IntrinsicElements[E]) => HTMLProps<E>;
 
-export type PolymorphicProps<E extends ElementType> = {
+export type PolymorphicProps<E extends ElementType = any> = {
   /**
    * The children snippet of the component.
    */
@@ -30,5 +30,5 @@ export type PolymorphicProps<E extends ElementType> = {
 };
 
 export type MaybeTracked<T> = {
-  [K in keyof T]?: T[K] | Tracked<T[K]>;
+  [K in keyof T]: T[K] | Tracked<T[K]>;
 };
